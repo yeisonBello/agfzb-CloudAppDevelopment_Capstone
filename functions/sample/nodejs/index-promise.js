ab688c7f6d0fe8278f523c4827b0d89f48d608ab
yeisonBello <yeisonbello.c@gmail.com>
2023-10-26 10:38:25 -0400

    upgrade in the function files

--- functions/sample/nodejs/index-promise.js
+++ functions/sample/nodejs/index-promise.js
@@ -1,66 +1,61 @@
 /**
- * Get all dealerships
+ * Get all dealerships and dealership by id 
  */
 
 const { CloudantV1 } = require('@ibm-cloud/cloudant');
 const { IamAuthenticator } = require('ibm-cloud-sdk-core');
 
 function main(params) {
-
-    const authenticator = new IamAuthenticator({ apikey: params.IAM_API_KEY })
+    const authenticator = new IamAuthenticator({ apikey: params.IAM_API_KEY });
     const cloudant = CloudantV1.newInstance({
-      authenticator: authenticator
+        authenticator: authenticator
     });
     cloudant.setServiceUrl(params.COUCH_URL);
 
-    let dbListPromise = getDbs(cloudant);
-    return dbListPromise;
+    const state = params.state;
+    if (state) {
+        return getAllDealerships(cloudant, "dealerships", state);
+    } else {
+        return getAllDealerships(cloudant, "dealerships");
+    }
 }
 
-function getDbs(cloudant) {
-     return new Promise((resolve, reject) => {
-         cloudant.getAllDbs()
-             .then(body => {
-                 resolve({ dbs: body.result });
-             })
-             .catch(err => {
-                  console.log(err);
-                 reject({ err: err });
-             });
-     });
- }
- 
- 
- /*
- Sample implementation to get the records in a db based on a selector. If selector is empty, it returns all records. 
- eg: selector = {state:"Texas"} - Will return all records which has value 'Texas' in the column 'State'
- */
- function getMatchingRecords(cloudant,dbname, selector) {
-     return new Promise((resolve, reject) => {
-         cloudant.postFind({db:dbname,selector:selector})
-                 .then((result)=>{
-                   resolve({result:result.result.docs});
-                 })
-                 .catch(err => {
-                    console.log(err);
-                     reject({ err: err });
-                 });
-          })
- }
- 
-                        
- /*
- Sample implementation to get all the records in a db.
- */
- function getAllRecords(cloudant,dbname) {
-     return new Promise((resolve, reject) => {
-         cloudant.postAllDocs({ db: dbname, includeDocs: true, limit: 10 })            
-             .then((result)=>{
-               resolve({result:result.result.rows});
-             })
-             .catch(err => {
-                console.log(err);
-                reject({ err: err });
-             });
-         })
- }
+function getAllDealerships(cloudant, dbname, state) {
+    return new Promise((resolve, reject) => {
+        cloudant.postAllDocs({ db: dbname, includeDocs: true, limit: 10 })
+            .then(result => {
+                let filteredResults = result.result.rows;
+                if (state) {
+                    filteredResults = filteredResults.filter(row => {
+                        const doc = row.doc;
+                        return doc.state === state;
+                    });
+                }
+
+                if (filteredResults.length === 0) {
+                    // Handle no matching state scenario (HTTP 404)
+                    reject({ status: 404, message: "The state does not exist" });
+                } else {
+                    const formattedResults = filteredResults.map(row => {
+                        const doc = row.doc;
+                        return {
+                            id: doc.id,
+                            city: doc.city,
+                            state: doc.state,
+                            st: doc.st,
+                            address: doc.address,
+                            zip: doc.zip,
+                            lat: doc.lat,
+                            long: doc.long
+                        };
+                    });
+                    resolve({ result: formattedResults });
+                }
+            })
+            .catch(err => {
+                // Handle server error scenario (HTTP 500)
+                console.error(err);
+                reject({ status: 500, message: "Something went wrong on the server" });
+            });
+    });
+}
--- functions/sample/python/main.py
+++ functions/sample/python/main.py
@@ -9,27 +9,81 @@ import requests
 
 
 def main(param_dict):
-    """Main Function
-
-    Args:
-        param_dict (Dict): input paramater
-
-    Returns:
-        _type_: _description_ TODO
-    """
-
+  
     try:
+        # Connect to the Cloudant database
         client = Cloudant.iam(
             account_name=param_dict["COUCH_USERNAME"],
             api_key=param_dict["IAM_API_KEY"],
             connect=True,
         )
-        print(f"Databases: {client.all_dbs()}")
-    except CloudantException as cloudant_exception:
-        print("unable to connect")
-        return {"error": cloudant_exception}
-    except (requests.exceptions.RequestException, ConnectionResetError) as err:
-        print("connection error")
-        return {"error": err}
 
-    return {"dbs": client.all_dbs()}
+        # Get a specific database (replace 'reviews' with your actual database name)
+        database = client['reviews']
+
+        # Retrieve reviews for the specified dealerId
+        dealer_id = param_dict.get("dealerId")
+        reviews = []
+        for document in database:
+            if document.get("id") == dealer_id:
+                reviews.append(document)  # Assuming each document in the 'reviews' database represents a review for a specific dealerId
+
+        # If no reviews found, return 404 error
+        if not reviews:
+            return {"error": "DealerId does not exist"}, 404
+
+        # Return the list of reviews for the specified dealership
+        return {"reviews": reviews}
+
+    
+    except CloudantException as cloudant_exception:
+        print("Error connecting to the database")
+        return {"error": "Something went wrong on the server"}, 500
+    except (requests.exceptions.RequestException, ConnectionResetError) as err:
+        print("Connection error")
+        return {"error": "Something went wrong on the server"}, 500
+
+    finally:
+        # Always remember to disconnect from the database after the operation
+        client.disconnect()
+
+
+
+def post_review(param_dict):
+    """Function to post a review for a dealership"""
+
+    try:
+        # Connect to the Cloudant database
+        client = Cloudant.iam(
+            account_name=param_dict["COUCH_USERNAME"],
+            api_key=param_dict["IAM_API_KEY"],
+            connect=True,
+        )
+
+        # Get a specific database (replace 'reviews' with your actual database name)
+        database = client['reviews']
+
+        # Extract review data from the request parameters
+        review_data = param_dict.get("review")
+        dealer_id = review_data.get("dealership")
+        
+        # Check if the dealership exists
+        if not get_reviews(dealer_id, database):
+            return {"error": "DealerId does not exist"}, 404
+
+        # Insert the review into the database
+        document = database.create_document(review_data)
+
+        # Return success response
+        return {"message": "Review posted successfully", "reviewId": document["_id"]}, 201  # 201 Created status code
+
+    except CloudantException as cloudant_exception:
+        print("Error connecting to the database")
+        return {"error": "Something went wrong on the server"}, 500
+    except (requests.exceptions.RequestException, ConnectionResetError) as err:
+        print("Connection error")
+        return {"error": "Something went wrong on the server"}, 500
+
+    finally:
+        # Always remember to disconnect from the database after the operation
+        client.disconnect()
